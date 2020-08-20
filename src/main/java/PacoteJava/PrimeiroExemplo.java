beed1de3cbeff1b459a5ef7710d6983ec4f4e7ae
César Ng <csantanang@gmail.com>
2020-08-20 11:33:09 -0300

    iniciando o projeto java com o segundo h

---
+++ src/main/java/PacoteJava/PrimeiroExemplo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PacoteJava;
+
+/**
+ *
+ * @author CESAR
+ */
+public class PrimeiroExemplo {
+    public static void main(String arqs[]){
+        System.out.println("ola mundo!");
+        
+        SegundoExemplo.escrever();
+    }
+    
+}
---
+++ src/main/java/PacoteJava/SegundoExemplo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PacoteJava;
+
+/**
+ *
+ * @author CESAR
+ */
+public class SegundoExemplo {
+     
+   public static void escrever(){
+       int x = 10;
+       String nome = "Valor = ";
+       System.out.println(nome + " " + x);
+            
+   } 
+      
+}
